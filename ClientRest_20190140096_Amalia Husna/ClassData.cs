0ff2b2a36456cdca2f8eb41567ba2d4c9ba6c928
Husna Amalia <amalia.husna.ft19@mail.umy.ac.id>
2021-12-30 22:45:07 +0700

    Add project files.

---
+++ ClientRest_20190140096_Amalia Husna/ClassData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_20190140096_Amalia_Husna.Program;
+using System.Collections.Generic;
+
+namespace ClientRest_20190140096_Amalia_Husna
+{
+    internal class ClassData
+    {
+        internal void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
